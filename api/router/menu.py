b625c113f43aeba093357a9f20c3f82a34ce4548
NathanDraco22 <nathandraco22@gmai.com>
2023-08-13 19:51:49 -0600

    upload image

--- api/router/menu.py
+++ api/router/menu.py
@@ -1,5 +1,7 @@
-from typing import Annotated
-from fastapi import APIRouter, UploadFile
+import os
+import shutil
+from urllib.parse import urlsplit, urljoin
+from fastapi import APIRouter, UploadFile, Request
 
 menu_router = APIRouter(prefix="/menu")
 
@@ -8,9 +10,18 @@ async def get_all_menu_items():
     return "ALL ITEM HERE"
 
 @menu_router.post("/upload-image")
-async def upload_menu_image(file: UploadFile):
-    print("esta llegando")
-    return {"file received" : file.filename}
+async def upload_menu_image(req: Request,file: UploadFile):
+    dir_path = os.path.join(os.getcwd(), "static", "images", file.filename)
+    with open(dir_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    splited = urlsplit(req.url._url)
+    base = f"{splited[0]}://{splited[1]}"
+    img_path = f"/assets/images/{file.filename}"
+    img_url = urljoin(base,img_path)
+    return {
+        "url" : img_url,
+        "file received" : file.filename
+    }
 
 
 
